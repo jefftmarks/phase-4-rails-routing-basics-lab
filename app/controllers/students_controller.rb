46b5de34a93a0c57fd8227216577528508a6d104
Jeff Marks <jeff.t.marks@gmail.com>
2022-09-18 14:30:19 -0400

    done with assignment

---
+++ app/controllers/students_controller.rb
@@ -0,0 +1,13 @@
+class StudentsController < ApplicationController
+
+	def index
+		students = Student.all
+		render json: students
+	end
+
+	def grades
+		students_by_grade = Student.order(grade: :desc)
+		render json: students_by_grade
+	end
+
+end
